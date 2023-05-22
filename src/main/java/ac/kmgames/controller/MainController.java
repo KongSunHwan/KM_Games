98e2b58e003c4ae74a971a7b019d32f26ba9e788
LeinneSW <nno88551@naver.com>
2023-05-22 15:50:06 +0900

    Changed the function so that MainController is responsible for handling the function being prepared

--- src/main/java/ac/kmgames/controller/MainController.java
+++ src/main/java/ac/kmgames/controller/MainController.java
@@ -4,16 +4,27 @@ import ac.kmgames.model.entity.User;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController{
     @GetMapping("/")
-    public String index(HttpSession session){
+    public String index(){
         return "redirect:/home";
     }
 
     @GetMapping("/home")
-    public String home(HttpSession session){
+    public String home(){
         return "home";
+    }
+
+    @ResponseBody
+    @GetMapping("*")
+    public String preparedFeature(){
+        return
+            "<script>" +
+                "alert('현재 준비중인 기능입니다');" +
+                "history.back();" +
+            "</script>";
     }
 }
