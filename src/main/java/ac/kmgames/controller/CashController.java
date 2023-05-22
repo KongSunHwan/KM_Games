57818207fc47e74911362b3733ed1bb62229d8ca
LeinneSW <nno88551@naver.com>
2023-05-22 15:42:57 +0900

    Added CashController

---
+++ src/main/java/ac/kmgames/controller/CashController.java
@@ -0,0 +1,13 @@
+package ac.kmgames.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CashController{
+    /*@GetMapping("/cash_history")
+    public String cashHistory(){
+        return "cash_history";
+    }*/
+}
