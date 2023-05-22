a71515acfc931a0ba80593b4d25d78f8061146e9
LeinneSW <nno88551@naver.com>
2023-05-22 16:05:15 +0900

    Disabled Spring Boot's default logout feature

--- src/main/java/ac/kmgames/config/SecurityConfig.java
+++ src/main/java/ac/kmgames/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.cors().disable().csrf().disable()
+                .logout().disable()
                 .formLogin().disable()
                 .authorizeHttpRequests()
                 .anyRequest().permitAll()
