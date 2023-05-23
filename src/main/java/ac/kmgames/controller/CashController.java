73d8f9cdb03633c17c48511cb9ac3b76718022dd
LeinneSW <nno88551@naver.com>
2023-05-23 10:50:12 +0900

    Changed wrong file name

--- src/main/java/ac/kmgames/controller/CashController.java
+++ src/main/java/ac/kmgames/controller/CashController.java
@@ -1,13 +1,31 @@
 package ac.kmgames.controller;
 
+import ac.kmgames.model.entity.User;
+import ac.kmgames.service.CashService;
+import ac.kmgames.service.PaymentService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CashController{
-    /*@GetMapping("/cash_history")
-    public String cashHistory(){
-        return "cash_history";
-    }*/
+    private final CashService cashService;
+    private final PaymentService paymentService;
+
+    @Autowired
+    public CashController(CashService cashService, PaymentService paymentService){
+        this.cashService = cashService;
+        this.paymentService = paymentService;
+    }
+
+    @GetMapping("/payment_history")
+    public String myPage(HttpSession session, HttpServletRequest request){
+        var obj = session.getAttribute("user");
+        if(obj instanceof User user){
+            request.setAttribute("payment_history", paymentService.getAllHistoryByEmail(user.getEmail()));
+        }
+        return "my_page";
+    }
 }
--- src/main/webapp/WEB-INF/view/cash/payment_history.jsp
+++ src/main/webapp/WEB-INF/view/cash/payment_history.jsp
@@ -1,6 +1,5 @@
 <%@ page import="ac.kmgames.model.entity.User" %>
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-         pageEncoding="UTF-8"%>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
 <%
     User user;
